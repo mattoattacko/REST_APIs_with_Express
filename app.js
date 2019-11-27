5a9b6c4c27732300738226ecf5e65a93d98b6f40
Matto <matt.mcquain@gmail.com>
2019-11-26 20:59:59 -0800

    basic routing started

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.get('/greetings', (req, res) => {
+  res.json({greeting: 'Hellow World!'})
+});
+
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
