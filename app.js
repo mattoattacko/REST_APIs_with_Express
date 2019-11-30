76a424f7e8b6cbfaf8f3eb67c06c04a3f2e22d79
Matto <matt.mcquain@gmail.com>
2019-11-29 20:31:38 -0800

    added routing plans

--- app.js
+++ app.js
@@ -5,4 +5,16 @@ app.get('/greetings', (req, res) => {
   res.json({greeting: 'Hellow World!'})
 });
 
+/**
+ * General Plan
+ */
+
+// Send a GET request to /quotes to READ a list of quotes
+// Send a GET request to /quotes/:id to READ (view) a quote
+// Send a POST request to /quotes to CREATE a new quote
+// Send a PUT request to quotes/:id to UPDATE (edit) a quote
+// Send a DELETE request to /quotes/:id to DELETE a quote
+// Send a GET request to /quotes/quote/random to READ (view) a random quote
+
+
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
