c60783ed6e0cb25e041612dc4bc88039e99ab437
Matto <matt.mcquain@gmail.com>
2019-12-01 20:59:36 -0800

    refactored the get all quotes route

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const app = express();
 
+const records = require('./records');
 
 // Send a GET request to /quotes to READ a list of quotes
 app.get('/quotes', (req, res)=>{
-  res.json(data);
+  const quotes = records.getQuotes();
+  res.json(quotes);
 });
 
 // Send a GET request to /quotes/:id to READ(view) a quote
 app.get('/quotes/:id', (req, res)=>{
-  const quote = data.quotes.find(quote => quote.id == req.params.id);
-  res.json(quote);
+
 });
 
 
@@ -20,23 +21,3 @@ app.get('/quotes/:id', (req, res)=>{
 // Send a GET request to /quotes/quote/random to READ (view) a random quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
-
-const data = {
-    quotes: [
-      {
-        id: 8721,
-        quote: "We must accept finite disappointment, but we must never lose infinite hope.",
-        author: "Martin Luther King"
-      },
-      {
-        id: 5779,
-        quote: "Use what you’ve been through as fuel, believe in yourself and be unstoppable!",
-        author: "Yvonne Pierre"
-      },
-      {
-        id: 3406,
-        quote: "To succeed, you have to do something and be very bad at it for a while. You have to look bad before you can look really good.",
-        author: "Barbara DeAngelis"
-      }
-    ]
-  }
