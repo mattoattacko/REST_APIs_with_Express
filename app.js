1e5611078e5a4f394153fd7873c65b3451ff9c06
Matto <matt.mcquain@gmail.com>
2019-12-07 20:08:08 -0800

    added error handling

--- app.js
+++ app.js
@@ -13,17 +13,33 @@ app.get('/quotes', async (req, res)=>{
 
 // Send a GET request to /quotes/:id to READ(view) a quote
 app.get('/quotes/:id', async (req, res)=>{
-  const quote = await records.getQuote(req.params.id);
-  res.json(quote);
+  try {
+    const quote = await records.getQuote(req.params.id);
+    if(quote){
+      res.json(quote);
+    } else {
+      res.status(404).json({message: "Quote not found."});
+    }
+  } catch(err) {
+      res.status(500).json({message: err.message});
+  }
 });
 
 // Send a POST request to /quotes to  CREATE a new quote 
-app.post('/quotes', async (req,res) => {
-  const quote = await records.createQuote({
-    quote: req.body.quote,
-    author: req.body.author
-  });
-  res.json(quote);
+app.post('/quotes', async (req,res) =>{
+  try {
+    if(req.body.author && req.body.quote){
+      const quote = await records.createQuote({
+        quote: req.body.quote,
+        author: req.body.author
+      });
+        res.status(201).json(quote);
+      } else {
+        res.status(400).json({message: "Quote and author required."});
+      }
+  } catch(err) {
+      res.status(500).json({message: err.message});
+  } 
 });
 
 
