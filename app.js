e36d804423ead36eb48dfee9f3569083b8c6541d
Matto <matt.mcquain@gmail.com>
2019-12-09 21:08:34 -0800

    worked on global error handling

--- app.js
+++ app.js
@@ -73,6 +73,24 @@ app.delete('/quotes/:id', async (req, res) => {
     res.status(500).json({message: err.message});
   }
 });
+
+
 // Send a GET request to /quotes/quote/random to READ (view) a random quote
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      message: err.message
+    }
+  })
+});
+
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
