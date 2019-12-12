5533ab6619775adbf1ac3aa5a8c06774dec90318
Matto <matt.mcquain@gmail.com>
2019-12-11 20:50:28 -0800

    modularized routing

--- app.js
+++ app.js
@@ -1,97 +1,9 @@
 const express = require('express');
 const app = express();
-
-const records = require('./records');
-
-function asyncHandler(cb) {
-  return async (req, res, next) => {
-    try {
-      await cb(req, res, next);
-    } catch(err) {
-        next(err);
-    }
-  }
-}
+const routes = require('./routes');
 
 app.use(express.json());
-
-// Send a GET request to /quotes to READ a list of quotes
-app.get('/quotes', async (req, res)=>{
-  const quotes = await records.getQuotes();
-  res.json(quotes);
-});
-
-// Send a GET request to /quotes/:id to READ(view) a quote
-app.get('/quotes/:id', async (req, res)=>{
-  try {
-    const quote = await records.getQuote(req.params.id);
-    if(quote){
-      res.json(quote);
-    } else {
-      res.status(404).json({message: "Quote not found."});
-    }
-  } catch(err) {
-      res.status(500).json({message: err.message});
-  }
-});
-
-// Send a POST request to /quotes to  CREATE a new quote 
-// app.post('/quotes', async (req,res) =>{
-//   try {
-//     if(req.body.author && req.body.quote){
-//       const quote = await records.createQuote({
-//         quote: req.body.quote,
-//         author: req.body.author
-//       });
-//         res.status(201).json(quote);
-//       } else {
-//         res.status(400).json({message: "Quote and author required."});
-//       }
-//   } catch(err) {
-//       res.status(500).json({message: err.message});
-//   } 
-// });
-
-app.post('/quotes', asyncHandler( async (req, res) => {
-    if(req.body.author && req.body.quote){
-      const quote = await records.createQuote({
-        quote: req.body.quote,
-        author: req.body.author
-      });
-        res.status(201).json(quote);
-      } else {
-        res.status(400).json({message: "Quote and author required."});
-      }
-}));
-
-// Send a PUT request to /quotes/:id to UPDATE (edit) a quote
-app.put('/quotes/:id', asyncHandler (async (req, res) => {
-  const quote = await records.getQuote(req.params.id);
-    if(quote){
-      quote.quote = req.body.quote;
-      quote.author = req.body.author;
-
-      await records.updateQuote(quote);
-      res.status(204).end();
-    } else {
-      res.status(404).json({message: "Quote Not Found"});
-    }
-}));
-
-
-// Send a DELETE request to /quotes/:id DELETE a quote 
-app.delete('/quotes/:id', async (req, res) => {
-  try {
-    const quote = await records.getQuote(req.params.id);
-    await records.deleteQuote(quote);
-    res.status(204).end();
-  } catch(err) {
-    res.status(500).json({message: err.message});
-  }
-});
-
-
-// Send a GET request to /quotes/quote/random to READ (view) a random quote
+app.use('/api', routes);
 
 app.use((req, res, next) => {
   const err = new Error("Not Found");
